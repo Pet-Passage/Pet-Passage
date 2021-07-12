1d345ea840200e2c3b66a10d79b1e924863fd43c
atramirez <adenramirez166@gmail.com>
2021-07-12 13:42:38 -0700

    updated to follow style, and names

--- lib/magnetic_sensor.hpp
+++ lib/magnetic_sensor.hpp
@@ -1,15 +1,15 @@
-#ifndef MAGNETIC_SENSOR
-#define MAGNETIC_SENSOR
+#ifndef PET_PASSAGE_LIB_MAGNETIC_SENSOR_H
+#define PET_PASSAGE_LIB_MAGNETIC_SENSOR_H
 
 #include <Arduino.h>
 
 template <int Pin>
 class MagneticSensor {
  public:
-  void updateRead();               // this will update the magState
-  unsigned short getUpdateRead();  // this will update and return a read
-  unsigned short getRead();        // this will return the current read
-  void printRead();                // this will print it for testing purposes
+  void updateState();               // this will update the magState
+  unsigned short getUpdateState();  // this will update and return a read
+  unsigned short getState();        // this will return the current read
+  void printState();                // this will print it for testing purposes
 
  private:
   unsigned short magState;  // stores the current state of a mag sensor
@@ -21,28 +21,28 @@ class MagneticGroup {
 };
 
 template<int Pin>
-void MagneticSensor<Pin>::updateRead()
+void MagneticSensor<Pin>::updateState()
 {
     this->magState = digitalRead(Pin);
 }
 
 template<int Pin>
-unsigned short MagneticSensor<Pin>::getUpdateRead()
+unsigned short MagneticSensor<Pin>::getUpdateState()
 {
     this->magState = digitalRead(Pin);
     return magState;
 }
 
 template<int Pin>
-unsigned short MagneticSensor<Pin>::getRead()
+unsigned short MagneticSensor<Pin>::getState()
 {
     return magState;
 }
 
 template<int Pin>
-void MagneticSensor<Pin>::printRead()
+void MagneticSensor<Pin>::printState()
 {
     Serial.println(getRead());
 }
 
-#endif  // MAGNETIC_SENSOR
+#endif  // PET_PASSAGE_LIB_MAGNETIC_SENSOR_H
